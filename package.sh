bf248f3448b0a8023ba9de359810c68e1de2a10b
gjdaniel1999 <gjdaniel1999@gmail.com>
2016-10-08 17:46:06 +0530

    Update binaries

--- package.sh
+++ package.sh
@@ -3,11 +3,15 @@ export CGO_ENABLED=0
 
 go build -o bin/urban_linux32
 go build -o bin/urban_linux64
+echo "Linux build done."
+
 
 # Mac
 env GOOS=darwin GOARCH=386 go build -o bin/urban_darwin32
 env GOOS=darwin GOARCH=amd64 go build -o bin/urban_darwin64
+echo "Mac build done."
 
 # Windows
 env GOOS=windows GOARCH=386 go build -o bin/urban_win32.exe
 env GOOS=windows GOARCH=amd64 go build -o bin/urban_win64.exe
+echo "Windows build done."
